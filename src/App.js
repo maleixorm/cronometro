904d47d315c2953574e8591cf4b8784179e9ade1
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-12 14:59:09 -0300

    Criando a estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return(
+            <div className='container'>
+                <img src={require('./assets/cronometro.png')} className='img' alt='cronômetro' />
+                <a className='timer'>0.0</a>
+                <div className='areaBtn'>
+                    <a className='botao'>Iniciar</a>
+                    <a className='botao'>Zerar</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
